9cd59852d42c8fafa13dfc33c936911377056629
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-08-06 13:32:36 -0400

    feat: Implement Education component

---
+++ cv-application/src/components/Education.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+function Education() {
+  const [educationData, setEducation] = useState({
+    university: "",
+    degree: "",
+    endDate: "",
+  });
+  const [isEditing, setEditing] = useState(true);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setEducation((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setEditing(false);
+  }
+
+  if (isEditing) {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h2>Education</h2>
+          <label>
+            University:
+            <input
+              type="text"
+              name="university"
+              value={educationData.university}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Degree:
+            <input
+              type="text"
+              name="degree"
+              value={educationData.degree}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Graduation Date:
+            <input
+              type="date"
+              name="endDate"
+              value={educationData.endDate}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2>Education</h2>
+      <p>University: {educationData.university}</p>
+      <p>Degree: {educationData.degree}</p>
+      <p>Graduation Date: {educationData.endDate}</p>
+      <button onClick={() => setEditing(true)}>Edit</button>
+    </div>
+  );
+}
+
+export default Education;
