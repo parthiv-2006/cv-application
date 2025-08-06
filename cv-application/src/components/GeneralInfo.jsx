2168e682c2e54561dcd9acfe3d7705b2d0513280
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-08-06 13:17:28 -0400

    fix: Typo in setEditing name

--- cv-application/src/components/GeneralInfo.jsx
+++ cv-application/src/components/GeneralInfo.jsx
@@ -1,49 +1,66 @@
 import { useState } from "react";
 
 function GeneralInfo() {
-    const [formData, setFormData] = useState({name: '', email: '', phone: ''})
-    const [isEditing, setEdting] = useState(true)
+  const [formData, setFormData] = useState({ name: "", email: "", phone: "" });
+  const [isEditing, setEditing] = useState(true);
 
-    function handleChange(event) {
-        const {name, value} = event.target
-        setFormData(prevFormData => ({...prevFormData, [name]: value}))
-    }
-    function handleSubmit(event) {
-        event.preventDefault()
-        setEdting(false)
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    setEditing(false);
+  }
 
-    if (isEditing) {
-        return (
-        <div className="general-info-section">
-            <h2>General Information</h2>
-            <form onSubmit={handleSubmit}>
-            <label>
-                Name*:
-                <input type="text" name="name" value={formData.name} onChange={handleChange} required/>
-            </label>
-            <label>
-                E-mail*:
-                <input type="email" name="email" value={formData.email} onChange={handleChange} required/>
-            </label>
-            <label>
-                Phone Number:
-                <input type="tel" name="phone" value={formData.phone} onChange={handleChange}/>
-            </label>
-            <button type="submit">Submit</button>
-            </form>
-        </div>
-        );
-    }
+  if (isEditing) {
     return (
-      <div>
+      <div className="general-info-section">
         <h2>General Information</h2>
-        <p>Name: {formData.name}</p>
-        <p>E-mail: {formData.email}</p>
-        {formData.phone !== '' && <p>Phone: {formData.phone}</p>}
-        <button onClick={() => setEdting(true)}>Edit</button>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name*:
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            E-mail*:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Phone Number:
+            <input
+              type="tel"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
+  }
+  return (
+    <div>
+      <h2>General Information</h2>
+      <p>Name: {formData.name}</p>
+      <p>E-mail: {formData.email}</p>
+      {formData.phone !== "" && <p>Phone: {formData.phone}</p>}
+      <button onClick={() => setEditing(true)}>Edit</button>
+    </div>
+  );
 }
 
 export default GeneralInfo;
